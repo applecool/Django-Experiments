7ebdfbb6d5cf452fbe98c845c47fb3937681750b
applecool <kmitharsha@gmail.com>
2015-10-04 21:05:51 -0500

    added filtering

--- polls/admin.py
+++ polls/admin.py
@@ -13,5 +13,8 @@ class QuestionAdmin(admin.ModelAdmin):
 
 	list_display = ('question_text','pub_date','was_published_in_last_7_days')
 
+	list_filter = ['pub_date']
+	search_fields = ['question_text']
+	
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -15,6 +15,10 @@ class Question(models.Model):
 	def was_published_in_last_7_days(self):
 		return self.pub_date >=timezone.now() - datetime.timedelta(days=7)
 
+	was_published_in_last_7_days.admin_order_field = 'pub_date'
+	was_published_in_last_7_days.boolean = True
+	was_published_in_last_7_days.short_description = 'Published this Week'
+	
 class Choice(models.Model):
 
 	choice_text = models.CharField(max_length=200)
