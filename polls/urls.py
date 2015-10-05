9be7a7fe114b100c2bf13deba849ad0ee18d45e9
applecool <kmitharsha@gmail.com>
2015-10-04 22:12:13 -0500

    mapped the urls to the views

--- polls/urls.py
+++ polls/urls.py
@@ -2,4 +2,4 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns = [url(r'^$', views.index, name='index'),]
+urlpatterns = [url(r'^$', views.index, name='index'), url(r'^(?P<question_id>[0-9]+)/$', views.results, name='detail'), url(r'^(?P<question_id>[0-9]+)/results$', views.detail, name='results'), url(r'^(?P<question_id>[0-9]+)/vote$', views.vote, name='vote')]
