1851c15db86485ae666f258fcf16243319407f75
applecool <kmitharsha@gmail.com>
2015-10-08 17:19:12 -0500

    added past question test

--- polls/tests.py
+++ polls/tests.py
@@ -5,6 +5,7 @@ from .models import Question
 import datetime
 
 from django.utils import timezone
+from django.core.urlresolvers import reverse
 
 # Create your tests here.
 
@@ -21,3 +22,17 @@ class QuestionMethodTests(TestCase):
 		time = timezone.now() - datetime.timedelta(hours=1)
 		recent_question = Question(pub_date=time)
 		self.assertEqual(recent_question.was_published_in_last_7_days, True)
+
+
+class QuestionViewTests(TestCase):
+	def test_index_view_with_no_questions(self):
+		response = self.client.get(reverse('polls:index'))
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, "You haven't uploaded any questions yet.")
+		self.assertQuerysetEqual(response.context["latest_question_list"], [])
+
+class QuestionIndexDetailTests(TestCase):
+	def test_detail_view_with_a_past_question(self):
+		past_question = create_question(question_text="Past question", days=-5)
+		response = self.client.get(reverse('polls:detail', args=(past_question.id,)))
+		self.assertContains(response,past_question.question_text, status_code=200)
--- polls/views.py
+++ polls/views.py
@@ -11,6 +11,8 @@ from .models import Question, Choice
 
 from django.http import Http404
 
+from django.utils import timezone
+
 
 # Create your views here.
 
@@ -19,7 +21,7 @@ class IndexView(generic.ListView):
 	context_object_name = 'latest_question_list'
 
 	def get_queryset(self):
-		return Question.objects.order_by('-pub_date')[:5]
+		return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 #def index(request):
 	#latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -35,6 +37,9 @@ class DetailView(generic.DetailView):
 	model = Question
 	template_name = 'polls/detail.html'
 
+	def get_queryset(self):
+		return Question.objects.filter(pub_date__lte=timezone.now())
+
 #def detail(request, question_id):
 	#question = get_object_or_404(Question, pk=question_id)
 	#return HttpResponse("This is the detail view of the question: %s" % question_id)
@@ -48,6 +53,9 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
 	model = Question
 	template_name = 'polls/results.html'
+
+	def get_queryset(self):
+		return Question.objects.filter(pub_date__lte=timezone.now())
 
 #def results(request, question_id):
 	#question = get_object_or_404(Question, pk=question_id)
