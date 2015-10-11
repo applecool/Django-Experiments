9bd561d42551d18339fed8549b3ea8191c81b11c
applecool <kmitharsha@gmail.com>
2015-10-10 23:16:48 -0500

    clean imports

--- polls/tests.py
+++ polls/tests.py
@@ -11,18 +11,15 @@ from django.core.urlresolvers import reverse
 
 
 class QuestionMethodTests(TestCase):
-
 	def test_published_recently_with_future_question(self):
 		time = timezone.now() + datetime.timedelta(days=30)
 		future_question = Question(pub_date=time)
 		self.assertEqual(future_question.was_published_in_last_7_days(), False)
 
-
 	def test_published_recently_with_recent_question(self):
 		time = timezone.now() - datetime.timedelta(hours=1)
 		recent_question = Question(pub_date=time)
 		self.assertEqual(recent_question.was_published_in_last_7_days, True)
-
 
 class QuestionViewTests(TestCase):
 	def test_index_view_with_no_questions(self):
